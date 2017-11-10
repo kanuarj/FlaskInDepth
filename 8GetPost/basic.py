89facea99fd1fff1c0a125460e861966a497ed88
kanuarj <jimmy.lpf@gmail.com>
2017-11-10 12:50:11 +0530

    Update basic.py

--- 8GetPost/basic.py
+++ 8GetPost/basic.py
@@ -1,1 +1,25 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+
+
+def basic():
+    #request.method and request.form
+    if request.method == 'POST':
+        if request.form['name'] and request.form['pass']:
+            return 'Validation successful'
+        else:
+            return 'Validation unsuccessful'
+        return 'Hi'
+    
+    
+         
+    return render_template('def.html')
+        
+
+if __name__ == '__main__':
+    app.run()
 
